198564f7a5ae6f9bb8d626820961ea783506a46c
Sofya Filippova <467879@niuitmo.ru>
2024-12-04 15:48:48 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -8,25 +8,24 @@ def calc(fig, func, size):
                 return pi * size[0] ** 2
             else:
                 raise AssertionError("Invalid parameters for circle area")
-        
+
         elif fig == 'square':
             if len(size) == 1 and size[0] > 0:
                 return size[0] ** 2
             else:
                 raise AssertionError("Invalid parameters for square area")
-        
+
         elif fig == 'triangle':
             if len(size) == 3 and all(s > 0 for s in size):
                 a, b, c = size
-                # Проверка существования треугольника
                 if a + b > c and a + c > b and b + c > a:
                     s = (a + b + c) / 2
-                    return (s * (s - a) * (s - b) * (s - c)) ** 0.5  # Формула Герона
+                    return (s * (s - a) * (s - b) * (s - c)) ** 0.5
                 else:
                     raise AssertionError("Invalid triangle sides")
             else:
                 raise AssertionError("Invalid parameters for triangle area")
-        
+
         else:
             raise AssertionError("Unknown figure for area calculation")
 
@@ -36,21 +35,21 @@ def calc(fig, func, size):
                 return 2 * pi * size[0]
             else:
                 raise AssertionError("Invalid parameters for circle perimeter")
-        
+
         elif fig == 'square':
             if len(size) == 1 and size[0] > 0:
                 return 4 * size[0]
             else:
                 raise AssertionError("Invalid parameters for square perimeter")
-        
+
         elif fig == 'triangle':
             if len(size) == 3 and all(s > 0 for s in size):
                 return sum(size)
             else:
                 raise AssertionError("Invalid parameters for triangle perimeter")
-        
+
         else:
             raise AssertionError("Unknown figure for perimeter calculation")
-    
+
     else:
         raise AssertionError("Unknown function")
