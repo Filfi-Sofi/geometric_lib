119e2482b1036bba37d866581eff7349dec7a70c
Sofya Filippova <467879@niuitmo.ru>
2024-12-04 15:27:01 +0300

    Update test_circle.py

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,39 +1,38 @@
 import unittest
+from math import pi
 from circle import area, perimeter
-import math
 
 
 class TestCircle(unittest.TestCase):
     def test_area(self):
-        # Arrange (given)
         radius = 1
-
-        # Act (when)
-        result = area(radius)
-
-        # Assert (then)
-        self.assertEqual(result, math.pi)
-
-    def test_area_zero(self):
-        radius = 0
-
-        result = area(radius)
-
-        self.assertEqual(result, 0)
+        res = area(radius)
+        self.assertEqual(res, pi)
 
     def test_perimeter(self):
         radius = 1
+        res = perimeter(radius)
+        self.assertEqual(res, 2 * pi)
 
-        result = perimeter(radius)
-
-        self.assertEqual(result, 2 * math.pi)
+    def test_area_zero(self):
+        radius = 0
+        res = area(radius)
+        self.assertEqual(res, 0)
 
     def test_perimeter_zero(self):
         radius = 0
+        res = perimeter(radius)
+        self.assertEqual(res, 0)
 
-        result = perimeter(radius)
+    def test_area_neg(self):
+        radius = -1
+        with self.assertRaises(AssertionError):
+            area(radius)
 
-        self.assertEqual(result, 0)
+    def test_perimeter_neg(self):
+        radius = -1
+        with self.assertRaises(AssertionError):
+            perimeter(radius)
 
 
 if __name__ == '__main__':
